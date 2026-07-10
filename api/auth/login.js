966eb9fc7160a29a8b48b618963debf205ad3455
PremiumKmarket <james@tronicholdings.com>
2026-07-10 10:54:01 -0700

    Update login.js

--- api/auth/login.js
+++ api/auth/login.js
@@ -43,6 +43,7 @@ module.exports = async (req, res) => {
       companyName: user.company_name,
       isAdmin: user.is_admin,
       createdAt: user.created_at,
+      repName: user.rep_name,
       message: user.approved ? 'Logged in. 로그인되었습니다.' : 'Logged in. Prices will be visible once an admin approves your account. 다만 아직 관리자 승인 전이라 가격은 승인 후에 보입니다.',
     });
   } catch (err) {
